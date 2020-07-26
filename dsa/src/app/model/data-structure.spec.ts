05edf5dab2f61278e566e2f8d59ac1b3b8071687
codeGit <minhduc.gov@gmail.com>
2020-07-26 09:57:57 +0700

    init

---
+++ dsa/src/app/model/data-structure.spec.ts
@@ -0,0 +1,7 @@
+import { DataStructure } from './data-structure';
+
+describe('DataStructure', () => {
+  it('should create an instance', () => {
+    expect(new DataStructure()).toBeTruthy();
+  });
+});
