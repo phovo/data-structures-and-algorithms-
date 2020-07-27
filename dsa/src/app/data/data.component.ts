00956e1c16fc8bf92c256291f1fec02b66928b6f
codeGit <minhduc.gov@gmail.com>
2020-07-27 22:58:47 +0700

    sort decrease

--- dsa/src/app/data/data.component.ts
+++ dsa/src/app/data/data.component.ts
@@ -8,7 +8,7 @@ import { VehicleVariant } from '../model/vehicle-variant';
 })
 export class DataComponent implements OnInit {
 
-  vehicles: VehicleVariant[]=[];
+  vehicles: VehicleVariant[] = [];
   numArr: Number[]
 
   constructor() { }
@@ -16,6 +16,39 @@ export class DataComponent implements OnInit {
   ngOnInit(): void {
     this.numArr = [7, 3, 9, 4, 1];
     this.vehicles = [new VehicleVariant(5, "ford"), new VehicleVariant(4, "nissan"), new VehicleVariant(8, "toyota"), new VehicleVariant(6, "honda")]
+    this.onSort();
+    
   }
 
+  
+
+
+  // 7, 3, 9, 4, 1
+  // 1,3, 4, 7, 9
+
+
+  onSort() {
+    let out = 0;
+    while (out < this.numArr.length) {
+      let index = out;
+      while (index < this.numArr.length) {
+        if (this.numArr[out] < this.numArr[index]) {
+          let temp = this.numArr[index];//tem = A
+          this.numArr[index] = this.numArr[out] // A = B
+          this.numArr[out] = temp;// B = Temp
+        }
+        index++
+      }
+      out++
+    }
+    //   for (let out = 0; out < this.numArr.length; out++) {
+    //     for (let index = out; index < this.numArr.length; index++) {
+    //       if (this.numArr[out] > this.numArr[index]) {
+    //         let temp = this.numArr[index];//tem = A
+    //         this.numArr[index] = this.numArr[out] // A = B
+    //         this.numArr[out] = temp;// B = Temp
+    //       }
+    //     }
+    //   }
+  }
 }
